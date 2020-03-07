c88f1d61f1a8a1ee16791ff2eaf6eeb745704e6e
Nguyen Hai-Son <nhson17@apcs.vn>
2020-03-07 16:38:35 +0700

    TodoItems list

---
+++ react/todo-list/src/components/TodoItems.js
@@ -0,0 +1,15 @@
+import React, {
+    Component
+} from 'react';
+
+class TodoItem extends Component {
+    render() {
+        return ( 
+        <div className = "TodoItem" >
+            <p>HOHOHOHOHO</p>
+        </div>
+        )
+    }
+}
+
+export default TodoItem;
