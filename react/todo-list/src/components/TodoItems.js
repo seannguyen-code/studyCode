14d302d54f5416744a4941761d9cb136f7ffa260
Nguyen Hai-Son <nhson17@apcs.vn>
2020-03-17 13:36:16 +0700

    TodoItems 2

--- react/todo-list/src/components/TodoItems.js
+++ react/todo-list/src/components/TodoItems.js
@@ -1,18 +1,21 @@
 import React, { Component } from "react";
 import classNames from "classnames";
 import "./TodoItem.css";
+import checkImg from "../img/check.svg";
+import unCheckImg from "../img/uncheck.svg";
 
 class TodoItem extends Component {
   render() {
     const { item, onClick } = this.props;
-
+    let url = unCheckImg;
+    if (item.isComplete) url = checkImg;
     return (
       <div
-        onClick={onClick}
         className={classNames("TodoItem", {
           "TodoItem-complete": item.isComplete
         })}
       >
+        <img onClick={onClick} src={url} width={32} height={32} />
         <p>{this.props.item.title}</p>
       </div>
     );
