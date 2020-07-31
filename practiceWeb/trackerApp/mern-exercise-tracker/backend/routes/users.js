9bd3400be577e346c33e87b4d8db90cf87bf5612
Nguyen Hai-Son <nhson17@apcs.vn>
2020-08-01 01:14:10 +0700

    fix .gitignore

--- practiceWeb/trackerApp/mern-exercise-tracker/backend/routes/users.js
+++ practiceWeb/trackerApp/mern-exercise-tracker/backend/routes/users.js
@@ -10,7 +10,9 @@ router.route('/').get((req, res) => {
 router.route('/add').post((req, res) => {
 	const username = req.body.username;
 
-	const newUser = new User({ username });
+	const newUser = new User({
+		username,
+	});
 
 	newUser
 		.save()
