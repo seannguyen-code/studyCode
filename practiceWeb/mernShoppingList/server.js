5243a31e1cdfbef657d3d3e76e3c54a7d8374752
Nguyen Hai-Son <nhson17@apcs.vn>
2020-08-08 15:18:07 +0700

    [Init] mern_shopping_list project

---
+++ practiceWeb/mernShoppingList/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const itemsRoute = require('./routes/api/items');
+
+const app = express();
+
+app.use(express.json());
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to Mongo
+mongoose
+	.connect(db, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => console.log('MongoDB Connected... '))
+	.catch((err) => console.log('Error: ' + err));
+
+// Use Routes
+app.use('/api/items', itemsRoute);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
