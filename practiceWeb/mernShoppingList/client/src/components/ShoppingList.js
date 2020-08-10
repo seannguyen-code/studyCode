33a4fbc1740285d7ce95f7eb81af21dcd52b7471
Nguyen Hai-Son <nhson17@apcs.vn>
2020-08-10 19:19:04 +0700

    [Add] Shopping List Component & Transitions

---
+++ practiceWeb/mernShoppingList/client/src/components/ShoppingList.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import { v4 as uuid } from 'uuid';
+
+class ShoppingList extends Component {
+	state = {
+		items: [
+			{ id: uuid(), name: 'Ak-47' },
+			{ id: uuid(), name: 'M4A1-S' },
+			{ id: uuid(), name: 'AWP' },
+			{ id: uuid(), name: 'MP5' },
+		],
+	};
+
+	render() {
+		const { items } = this.state;
+		return (
+			<Container>
+				<Button
+					color="dark"
+					style={{ marginBottom: '2rem' }}
+					onClick={() => {
+						const name = prompt('Enter Item');
+						if (name) {
+							this.setState((state) => ({
+								items: [...state.items, { id: uuid(), name }],
+							}));
+						}
+					}}
+				>
+					Add Item
+				</Button>
+
+				<ListGroup>
+					<TransitionGroup className="shopping-list">
+						{items.map(({ id, name }) => (
+							<CSSTransition key={id} timeout={500} classNames="fade">
+								<ListGroupItem>
+									<Button
+										className="remove-btn"
+										color="danger"
+										size="sm"
+										onClick={() => {
+											this.setState((state) => ({
+												items: state.items.filter((item) => item.id !== id),
+											}));
+										}}
+									>
+										&times;
+									</Button>
+									{name}
+								</ListGroupItem>
+							</CSSTransition>
+						))}
+					</TransitionGroup>
+				</ListGroup>
+			</Container>
+		);
+	}
+}
+
+export default ShoppingList;
