03bc767de1a45db292f4f48dccbb580eb402d743
Nguyen Hai-Son <nhson17@apcs.vn>
2020-08-17 01:12:50 +0700

    Redux

--- practiceWeb/mernShoppingList/client/src/components/ShoppingList.js
+++ practiceWeb/mernShoppingList/client/src/components/ShoppingList.js
@@ -1,37 +1,23 @@
 import React, { Component } from 'react';
 import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import { v4 as uuid } from 'uuid';
+import { connect } from 'react-redux';
+import { getItems, deleteItem } from '../actions/itemActions';
+import PropTypes from 'prop-types';
 
 class ShoppingList extends Component {
-	state = {
-		items: [
-			{ id: uuid(), name: 'Ak-47' },
-			{ id: uuid(), name: 'M4A1-S' },
-			{ id: uuid(), name: 'AWP' },
-			{ id: uuid(), name: 'MP5' },
-		],
+	componentDidMount() {
+		this.props.getItems();
+	}
+
+	onDeleteClick = (id) => {
+		this.props.deleteItem(id);
 	};
 
 	render() {
-		const { items } = this.state;
+		const { items } = this.props.item;
 		return (
 			<Container>
-				<Button
-					color="dark"
-					style={{ marginBottom: '2rem' }}
-					onClick={() => {
-						const name = prompt('Enter Item');
-						if (name) {
-							this.setState((state) => ({
-								items: [...state.items, { id: uuid(), name }],
-							}));
-						}
-					}}
-				>
-					Add Item
-				</Button>
-
 				<ListGroup>
 					<TransitionGroup className="shopping-list">
 						{items.map(({ id, name }) => (
@@ -41,11 +27,7 @@ class ShoppingList extends Component {
 										className="remove-btn"
 										color="danger"
 										size="sm"
-										onClick={() => {
-											this.setState((state) => ({
-												items: state.items.filter((item) => item.id !== id),
-											}));
-										}}
+										onClick={this.onDeleteClick.bind(this, id)}
 									>
 										&times;
 									</Button>
@@ -60,4 +42,13 @@ class ShoppingList extends Component {
 	}
 }
 
-export default ShoppingList;
+ShoppingList.propTypes = {
+	getItems: PropTypes.func.isRequired,
+	item: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+	item: state.item,
+});
+
+export default connect(mapStateToProps, { getItems, deleteItem })(ShoppingList);
