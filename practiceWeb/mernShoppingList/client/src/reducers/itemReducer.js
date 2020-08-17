ef1cef5917cabd581885f89ecb6c8214c5f6f870
Nguyen Hai-Son <nhson17@apcs.vn>
2020-08-17 22:34:51 +0700

    Connect BE and FE

--- practiceWeb/mernShoppingList/client/src/reducers/itemReducer.js
+++ practiceWeb/mernShoppingList/client/src/reducers/itemReducer.js
@@ -1,13 +1,14 @@
 import { v4 as uuid } from 'uuid';
-import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from '../actions/types';
+import {
+	GET_ITEMS,
+	ADD_ITEM,
+	DELETE_ITEM,
+	ITEMS_LOADING,
+} from '../actions/types';
 
 const initialState = {
-	items: [
-		{ id: uuid(), name: 'Ak-47' },
-		{ id: uuid(), name: 'M4A1-S' },
-		{ id: uuid(), name: 'AWP' },
-		{ id: uuid(), name: 'MP5' },
-	],
+	items: [],
+	loading: false,
 };
 
 export default function (state = initialState, action) {
@@ -15,18 +16,26 @@ export default function (state = initialState, action) {
 		case GET_ITEMS:
 			return {
 				...state,
+				items: action.payload,
+				loading: false,
 			};
 
 		case DELETE_ITEM:
 			return {
 				...state,
-				items: state.items.filter((item) => item.id !== action.payload),
+				items: state.items.filter((item) => item._id !== action.payload),
 			};
 
 		case ADD_ITEM:
 			return {
 				...state,
 				items: [action.payload, ...state.items],
+			};
+
+		case ITEMS_LOADING:
+			return {
+				...state,
+				loading: true,
 			};
 
 		default:
